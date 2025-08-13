95a7365680c025568061a98615682db2d6659d06
internDarshil-dmifotech <darshilkumar0353@gmail.com>
2025-08-13 23:16:51 +0530

    added animation to projects

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,22 +1,27 @@
-import GrainImage from "@/assets/images/grain.jpg";
-import { twMerge } from "tailwind-merge";
-import { PropsWithChildren } from "react";
+import { twMerge } from 'tailwind-merge';
+import GrainImage from '../assets/images/grain.jpg';
+import { ComponentPropsWithoutRef } from 'react';
 
-export const Card = ({className, children}:
-    PropsWithChildren<{
-    className?: string;
-}>) => {
-    return (
-        <div>
-            <div className={twMerge("bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none ",className)}
-            >
-                <div className="absolute inset-0 -z-10 opacity-5" style={{
-                    backgroundImage: `url(${GrainImage.src})`
-                }}>
-                </div>
-                {children}
-            </div>
-        </div>
-
-    )
-}
+export const Card = ({
+  className,
+  children,
+  ...other
+}: ComponentPropsWithoutRef<'div'>) => {
+  return (
+    <>
+      <div
+        className={twMerge(
+          "bg-gray-800 rounded-3xl relative after:content-[''] after:absolute after:inset-0 z-0 after:z-10 overflow-hidden after:oultine-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none",
+          className
+        )}
+        {...other}
+      >
+        <div
+          className='absolute inset-0 -z-10 opacity-5'
+          style={{ backgroundImage: `url(${GrainImage.src})` }}
+        ></div>
+        {children}
+      </div>
+    </>
+  );
+};
